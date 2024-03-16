76db1c27586c0f2a265437f951d747b766d43928
Pal Bijewar <pal.bijewar@gmail.com>
2024-03-17 01:03:19 +0530

    Initialised files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Header from "./components/Header";
+import Contact from "./pages/Contact";
+import Footer from "./pages/Footer";
+import HomePage from "./pages/HomePage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <HomePage />
+      <Contact />
+      <Footer/>
     </div>
   );
 }
